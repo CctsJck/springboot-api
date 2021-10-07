3e9b5a20f68ba8b4120ac079dab9fb75e0ef7293
gnb2000 <barirodriguez@gmail.com>
2021-10-07 17:35:23 -0300

    Act

--- src/main/java/ar/edu/uade/springapi/RestController.java
+++ src/main/java/ar/edu/uade/springapi/RestController.java
@@ -7,7 +7,6 @@ import java.util.List;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -25,8 +24,6 @@ import vo.ResponsableVO;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
-
-	System.out.println();
 
 
 	@RequestMapping("/hello")
