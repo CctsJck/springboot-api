085c21db8d58a3f1718c8c23ad9cba505454469a
gnb2000 <barirodriguez@gmail.com>
2021-10-19 14:12:57 -0300

    Agregado de CrossOrigin

--- src/main/java/ar/edu/uade/springapi/RestController.java
+++ src/main/java/ar/edu/uade/springapi/RestController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -33,6 +34,7 @@ public class RestController {
 	}
 	
 	@RequestMapping("/getJugadoresClub")
+	@CrossOrigin(origins="http://localhost:3000/")
 	public List<JugadorVO> getJugadoresClub(@RequestParam(name="idClub") int idClub) throws ClubException{
 		//Esta bien esto de la sesion aca?
 		SessionManager.getInstancia().openSession();
@@ -42,6 +44,7 @@ public class RestController {
 	}
 	
 	@RequestMapping("/getEstadisticaCampeonato")
+	@CrossOrigin(origins="http://localhost:3000/")
 	public String[][] getEstadisticasCampeonato(@RequestParam(name="idCampeonato") int idCampeonato) throws CampeonatoException{
 		SessionManager.getInstancia().openSession();
 		String[][] estadisticas = Controlador.getInstancia().getEstaditicaJugadoresCampeonato(idCampeonato);
@@ -51,25 +54,29 @@ public class RestController {
 	}
 	
 	@PostMapping("/agregarGolJugador")
+	@CrossOrigin(origins="http://localhost:3000/")
 	public void agregarGol(@RequestParam(name="idJugador") int idJugador,@RequestParam(name="idPartido") int idPartido,@RequestParam(name="minuto") int minuto, @RequestParam(name="tipo") String tipo) throws JugadorException, PartidoException, ClubException {
 		SessionManager.getInstancia().openSession();
 		Controlador.getInstancia().agregarGolJugador(idJugador, idPartido, minuto, tipo);
 		SessionManager.getInstancia().closeSession();
 	}
 	
-	@PostMapping ("/agregarFaltaJugador") 
+	@PostMapping ("/agregarFaltaJugador")
+	@CrossOrigin(origins="http://localhost:3000/")
 	public void agregarFaltaJugador(@RequestParam(name="idJugador") int idJugador,@RequestParam(name="idPartido") int idPartido,@RequestParam(name="idCampeonato") int idCampeonato, @RequestParam(name="minuto") int minuto, @RequestParam(name="tipo") String tipo) throws JugadorException, PartidoException, CampeonatoException, ClubException{
 		SessionManager.getInstancia().openSession();
 		Controlador.getInstancia().agregarFaltaJugador(idJugador, idPartido,idCampeonato, minuto, tipo);
 		SessionManager.getInstancia().closeSession();
 	}
 	@PostMapping("/crearClub")
+	@CrossOrigin(origins="http://localhost:3000/")
 	public void crearClub(@RequestParam(name="nombre") String nombre,@RequestParam("direccion") String direccion) {
 		SessionManager.getInstancia().openSession();
 		Controlador.getInstancia().crearClub(nombre, direccion);
 		SessionManager.getInstancia().closeSession();
 	}
 	@DeleteMapping("/eliminarClub")
+	@CrossOrigin(origins="http://localhost:3000/")
 	public void eliminarClub(@RequestParam(name="idClub") int idClub) throws ClubException {
 		SessionManager.getInstancia().openSession();
 		Controlador.getInstancia().eliminarClub(idClub);
@@ -77,54 +84,63 @@ public class RestController {
 	}
 	
 	@PutMapping("/modificarClub")
+	@CrossOrigin(origins="http://localhost:3000/")
 	public void modificarClub(@RequestParam(name="idClub") int idClub, @RequestParam(name="nombre") String nombre,@RequestParam(name="direccion") String direccion) throws ClubException {
 		SessionManager.getInstancia().openSession();
 		Controlador.getInstancia().modificarClub(idClub, nombre, direccion);
 		SessionManager.getInstancia().closeSession();
 	}
 	@PostMapping("/agregarJugador")
+	@CrossOrigin(origins="http://localhost:3000/")
 	public void agregarJugador(@RequestParam(name="tipoDocumento") String tipoDocumento, @RequestParam(name="documento") int documento,@RequestParam(name="nombre") String nombre, @RequestParam(name="apellido") String apellido, @RequestParam(name="idClub") int idClub, @RequestParam(name="fechaNacimiento") Date fechaNacimiento) throws ClubException {
 		SessionManager.getInstancia().openSession();
 		Controlador.getInstancia().agregarJugador(tipoDocumento, documento, nombre, apellido, idClub, fechaNacimiento);		
 		SessionManager.getInstancia().closeSession();
 	}
 	@DeleteMapping("/eliminarJugador")
+	@CrossOrigin(origins="http://localhost:3000/")
 	public void eliminarJugador(@RequestParam(name="idJugador") int idJugador) throws JugadorException, ClubException {
 		SessionManager.getInstancia().openSession();
 		Controlador.getInstancia().eliminarJugador(idJugador);
 		SessionManager.getInstancia().closeSession();
 	}
 	@PostMapping("/crearRepresentante")
+	@CrossOrigin(origins="http://localhost:3000/")
 	public void crearRepresentante(@RequestParam(name="tipoDocumento") String tipoDocumento, @RequestParam(name="DNI") int DNI, @RequestParam(name="nombre") String nombre, @RequestParam(name="idClub") int idClub) throws ClubException {
 		SessionManager.getInstancia().openSession();
 		Controlador.getInstancia().crearRepresentante(tipoDocumento, DNI, nombre, idClub);
 		SessionManager.getInstancia().closeSession();
 	}
 	@PutMapping("/modificarRepresentante")
+	@CrossOrigin(origins="http://localhost:3000/")
 	public void modificarRepresentante(@RequestParam(name="idRepresentante") int idRepresentante, @RequestParam(name="nombre") String nombre, @RequestParam(name="DNI") int DNI, @RequestParam(name="tipoDocumento") String tipoDocumento, @RequestParam(name="idClub") int idClub) throws ResponsableException, ClubException {
 		SessionManager.getInstancia().openSession();
 		Controlador.getInstancia().modificarRepresentante(idRepresentante, nombre, DNI, tipoDocumento, idClub);
 		SessionManager.getInstancia().closeSession();
 	}
 	@DeleteMapping("/eliminarRepresentante")
+	@CrossOrigin(origins="http://localhost:3000/")
 	public void eliminarRepresentante (@RequestParam(name="idRepresentante") int idRepresentante) throws ResponsableException, ClubException {
 		SessionManager.getInstancia().openSession();
 		Controlador.getInstancia().eliminarRepresentante(idRepresentante);
 		SessionManager.getInstancia().closeSession();
 	}
 	@PutMapping("/modificarJugador")
+	@CrossOrigin(origins="http://localhost:3000/")
 	public void modificarJugador(@RequestParam(name="idJugador") int idJugador, @RequestParam(name="tipoDocumento") String tipoDocumento, @RequestParam(name="numeroDocumento") int numeroDocumento, @RequestParam(name="nombre") String nombre, @RequestParam(name="apellido") String apellido, @RequestParam(name="idClub") int idClub, @RequestParam(name="fechaNac") Date fechaNac) throws JugadorException, ClubException {
 		SessionManager.getInstancia().openSession();
 		Controlador.getInstancia().modificarJugador(idJugador, tipoDocumento, numeroDocumento, nombre, apellido, idClub, fechaNac);
 		SessionManager.getInstancia().closeSession();
 	}
 	@PostMapping("/crearCampeonato")
+	@CrossOrigin(origins="http://localhost:3000/")
 	public void crearCampeonato(@RequestParam(name="descripcion") String descripcion, @RequestParam(name="fechaInicio") Date fechaInicio, @RequestParam(name="fechaFin") Date fechaFin, @RequestParam(name="tipo") String tipo, @RequestParam(name="categoria") int categoria) {
 		SessionManager.getInstancia().openSession();
 		Controlador.getInstancia().crearCampeonato(descripcion, fechaInicio, fechaFin, tipo, categoria);
 		SessionManager.getInstancia().closeSession();
 	}
 	@PostMapping("/agregarJugadorPartido")
+	@CrossOrigin(origins="http://localhost:3000/")
 	public void agregarJugadorPartido(@RequestParam(name="idPartido") int idPartido,@RequestParam(name="idJugador") int idJugador,@RequestParam(name="idClub") int idClub) throws ClubException, PartidoException, JugadorException {
 		SessionManager.getInstancia().openSession();
 		Controlador.getInstancia().agregarJugadorPartido(idPartido, idJugador, idClub);
@@ -132,6 +148,7 @@ public class RestController {
 	}
 	
 	@RequestMapping("/getResponsableClub")
+	@CrossOrigin(origins="http://localhost:3000/")
 	public List<ResponsableVO> getResponsableClub(@RequestParam(name="idClub") int idClub) throws ClubException{
 		SessionManager.getInstancia().openSession();
 		List<ResponsableVO> ResponsableVO = Controlador.getInstancia().getResponsableClub(idClub);
@@ -140,6 +157,7 @@ public class RestController {
 	}
 	
 	@PostMapping("/agregarClubCampeonato")
+	@CrossOrigin(origins="http://localhost:3000/")
 	public void agregarClubCampeonato(@RequestParam(name="idClub") int idClub, @RequestParam(name="idCampeonato") int idCampeonato) throws CampeonatoException, ClubException {
 		SessionManager.getInstancia().openSession();
 		Controlador.getInstancia().asignarParticipaciones(idClub, idCampeonato);
@@ -147,6 +165,7 @@ public class RestController {
 	}
 	
 	@PutMapping("/validarPartido")
+	@CrossOrigin(origins="http://localhost:3000/")
 	public void validarPartido(@RequestParam(name="idClub") int idClub, @RequestParam(name="idPartido") int idPartido) throws ClubException, PartidoException {
 		SessionManager.getInstancia().openSession();
 		Controlador.getInstancia().validarPartido(idClub, idPartido);
@@ -154,6 +173,7 @@ public class RestController {
 	}
 	
 	@DeleteMapping("/eliminarCampeonato")
+	@CrossOrigin(origins="http://localhost:3000/")
 	public void eliminarCampeonato(@RequestParam(name="idCampeonato") int idCampeonato) throws CampeonatoException {
 		SessionManager.getInstancia().openSession();
 		Controlador.getInstancia().eliminarCampeonato(idCampeonato);
@@ -168,6 +188,7 @@ public class RestController {
 	}*/
 	
 	@PostMapping("/crearPartido")
+	@CrossOrigin(origins="http://localhost:3000/")
 	public void crearPartido(@RequestParam(name="nroFecha") int nroFecha,@RequestParam(name="nroZona") int nroZona,@RequestParam(name="categoria") int categoria, @RequestParam(name="clubLocal") Integer clubLocal, @RequestParam(name="clubVisitante") Integer clubVisitante,@RequestParam(name="fechaPartido") Date fechaPartido,@RequestParam(name="idCampeonato") Integer idCampeonato) throws CampeonatoException, ClubException {
 		SessionManager.getInstancia().openSession();
 		Controlador.getInstancia().crearPartido(nroFecha, nroZona, categoria, clubLocal, clubVisitante, fechaPartido, idCampeonato);
@@ -175,6 +196,7 @@ public class RestController {
 	}
 	
 	@PostMapping("/crearPartidoAuto")
+	@CrossOrigin(origins="http://localhost:3000/")
 	public void crearPartidoAuto(@RequestParam(name="idCampeonato") Integer idCampeonato) throws CampeonatoException, ClubException {
 		SessionManager.getInstancia().openSession();
 		Controlador.getInstancia().crearPartidosNuevos(idCampeonato);
@@ -182,6 +204,7 @@ public class RestController {
 	}
 	
 	@PutMapping("/activarCampeonato")
+	@CrossOrigin(origins="http://localhost:3000/")
 	public void activarCampeonato(@RequestParam(name="idCampeonato") int idCampeonato) throws CampeonatoException {
 		SessionManager.getInstancia().openSession();
 		Controlador.getInstancia().activarCampeonato(idCampeonato);
@@ -189,6 +212,7 @@ public class RestController {
 	}
 	
 	@RequestMapping("/getPartidosTorneo")
+	@CrossOrigin(origins="http://localhost:3000/")
 	public List<PartidoVO> getPartidosCampeonato(@RequestParam(name="idCampeonato") int idCampeonato) throws CampeonatoException, ClubException {
 		SessionManager.getInstancia().openSession();
 		List<PartidoVO> partidos = Controlador.getInstancia().obtenerPartidosCampeonato(idCampeonato);
@@ -197,10 +221,11 @@ public class RestController {
 	}
 	
 	@RequestMapping("/obtenerCampeonatos")
+	@CrossOrigin(origins="http://localhost:3000/")
 	public List<CampeonatoVO> getCampeonatos(){
 		SessionManager.getInstancia().openSession();
 		List<CampeonatoVO> campeonatos = Controlador.getInstancia().obtenerCampeonatos();
-		SessionManager.getInstancia().closeSession();
+		SessionManager.getInstancia().closeSession(); 
 		return campeonatos;	
 	}
 }
